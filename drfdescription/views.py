ebeddca5179563e70a11bfff19968bfb43891e80
Jérémy Morel <jeremy.morel@owkin.com>
2020-01-30 09:41:33 +0100

    non-detail action

--- drfdescription/views.py
+++ drfdescription/views.py
@@ -4,10 +4,7 @@ from rest_framework.viewsets import GenericViewSet
 
 
 class DummyViewSet(GenericViewSet):
-    # queryset = Account.objects.all()
-    # serializer_class = AccountSerializer
-    # permission_classes = [IsAccountAdminOrReadOnly]
 
-    @action(detail=True)
+    @action(detail=False)
     def description(request, *args, **kwargs):
-        return Response('foo')
+        return Response('description')
